3029979f2136bdc311cb737dec93dea078183f40
Eitner <christian.eitner@siemens.com>
2019-06-10 02:41:46 +0200

    added Marker attribute

---
+++ src/System.Windows.Forms/src/Common/Marker.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Windows.Forms
+{
+    public class Marker : Attribute
+    {
+        IEnumerable<string> Data { get; set; }
+
+        public Marker()
+        {
+            Data = new List<string>();
+        }
+
+        public Marker(params string[] data)
+        {
+            Data = data;
+        }
+    }
+}
